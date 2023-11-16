1abf3b1df3f5928f3afa9ea765db119c6c3239ff
AronFrish <frishberg@uchicago.edu>
2023-11-16 12:13:45 -0600

    setting up automatic testing

--- tests/playground.py
+++
@@ -1,5 +0,0 @@
-from chess_analytica import Board, ChessDotCom
-
-profile = ChessDotCom.Profile("AKumarforchess", True)
-moves, f = profile.find_moves_after_FEN("r1bqkb1r/pppp1ppp/2n2n2/4p3/2B1P3/5N2/PPPP1PPP/RNBQK2R", True)
-print(str(f))
